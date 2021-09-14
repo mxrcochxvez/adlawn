b1b6303f6cfe3a83bbb1da478b3a5b740c51155b
“mxrcochxvez” <marcode.chavez.jr@gmail.com>
2021-09-14 10:38:15 -0700

    fixing text spacing

--- src/components/Splash.jsx
+++ src/components/Splash.jsx
@@ -5,10 +5,7 @@ export default function Splash() {
     return (
         <div className="bg-green-50 p-16 w-screen flex flex-col items-center">
             <img src={logo} alt="logo" className="w-80 mx-auto" />
-            <h1 className="text-5xl">Professional lawn care service you can</h1>{" "}
-            <h1 className="text-5xl">
-                <TextLoop className="text-green-800" children={["trust", "depend on", "rely on"]} />
-            </h1>
+            <h1 className="text-5xl">Professional lawn care service you can  <TextLoop className="text-green-800" children={["trust", "depend on", "rely on"]} /></h1>
             <p className="text-yellow-700">We offer premium Lawn Care services, read further to find out how to get in touch!</p>
         </div>
     )
